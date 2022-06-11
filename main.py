851ef23bea7530c9554fe2a5b9256a53a9f8c09b
Damian Jeździkowski <dejmienblue@gmail.com>
2022-06-11 14:28:37 +0200

    init

---
+++ main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, Depends, HTTPException, Request
+from auth import AuthHandler
+from schemas import AuthDetails
+from fastapi.templating import Jinja2Templates
+
+
+app = FastAPI()
+
+auth_handler = AuthHandler()
+templates = Jinja2Templates('./templates')
+users = []
+
+
+@app.post('/register', status_code=201)
+def register(auth_details: AuthDetails):
+    if any(x['username']==auth_details.username for x in users):
+        raise HTTPException(status_code=400, detail='Username is taken')
+    hashed_password = auth_handler.get_password_hash(auth_details.password)
+    users.append({
+        'username': auth_details.username,
+        'password': hashed_password
+    })
+    return {}
+
+@app.post('/login')
+def login(auth_details: AuthDetails):
+    user = None
+    for x in users:
+        if x['username'] == auth_details.username:
+            user = x
+            break
+        
+    if (user is None) or (not auth_handler.verify_password(auth_details.password, user['password'])):
+        raise HTTPException(status_code=401, detail='Invalid username and/or password')
+    
+    token = auth_handler.encode_token(user['username'])
+    return {'token': token}
+
+@app.get('/unprotected')
+def unprotected():
+    return {'hello': 'world'}
+
+@app.get('/protected')
+def protected(username=Depends(auth_handler.auth_wrapper)):
+    return {'username': username}
+
+
+@app.get('/home')   # is this correct?
+def home(request: Request, username=Depends(auth_handler.auth_wrapper)):
+    return templates.TemplateResponse('home.html', {'request': request, 'username': username})
+
